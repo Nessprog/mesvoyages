5331dec251940da7e909b23c3a4c554c7541054d
Nessprog <ineslaniez31@gmail.com>
2024-01-11 21:56:08 +0100

    Phase 11 : ORM (nouvelle page pour l'affichage détaillé d'un enregistrement)

--- src/Controller/VoyagesController.php
+++ src/Controller/VoyagesController.php
@@ -64,8 +64,20 @@ class VoyagesController extends AbstractController{
     public function findAllEqual($champ, Request $request): Response {
      $valeur = $request->get("recherche");
      $visites = $this->repository->findByEqualValue($champ, $valeur);
-     return $this->render("pages/voyages.html.twig",[ 
+     return $this->render("pages/voyage.html.twig",[ 
          'visites' => $visites
      ]);
+    }
+    
+    /**
+     * @Route("/voyages/voyage/{id}", name="voyages.showone")
+     * @param type $id
+     * @return Response
+     */
+    public function showOne($id): Response{
+        $visite = $this->repository->find($id);
+        return $this->render("pages/voyage.html.twig", [
+            'visite' => $visite
+        ]);
     }
 }
