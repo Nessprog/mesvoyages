37e67172f7a1b488682cc3569e626996aac7443c
Nessprog <ineslaniez31@gmail.com>
2024-01-11 15:42:45 +0100

    Phase 8 : ORM (affichage en colonne des informatons provenant de la BDD)

--- src/Entity/Visite.php
+++ src/Entity/Visite.php
@@ -81,9 +81,13 @@ class Visite
         return $this;
     }
 
-    public function getDatecreation(): ?\DateTimeInterface
+    public function getDatecreationString(): string
     {
-        return $this->datecreation;
+        if($this->datecreation == null){
+            return"";
+        }else{
+        return $this->datecreation->format('d/m/Y');
+        }
     }
 
     public function setDatecreation(?\DateTimeInterface $datecreation): self
